eb9ad91f464b57c12759a587ac68d3af8a98ecd4
5312 <2493024738@qq.com>
2024-04-05 11:46:40 +0800

    增加了tkinter文件的内容

--- tkinter01.py
+++ tkinter01.py
@@ -1,13 +1,15 @@
 import tkinter as tk
 
 window = tk.Tk()
-window.title('Label的使用')
-window.geometry('400x400')
+window.title('图书分类结果')
+window.geometry('640x640')
 
-label = tk.Label(window, text='test',
-                 bg='black', fg='white',  # bg为背景色，fg为前景色
+v = tk.StringVar()
+label = tk.Label(window, textvariable=v,
                  width=30, height=3,  # width为标签的宽，height为高
-wraplength = 150,  # 设置多少单位后开始换行
-anchor = 'w')  # 设置文本在标签中显示的位置
+                 font=24,
+                 wraplength=150,  # 设置多少单位后开始换行
+                 anchor='w')  # 设置文本在标签中显示的位置
+v.set("微积分：%d；线性代数：%d；数字逻辑与电路：%d；大学英语：%d；离散数学：%d" % (a, b, c, d, e))
 label.pack()
 window.mainloop()
