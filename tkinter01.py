8a32735546b9c78e7ab9553cba7f0a392ea88408
5312 <2493024738@qq.com>
2024-04-11 09:17:06 +0800

    修改了窗口界面的参数设置

--- tkinter01.py
+++ tkinter01.py
@@ -4,12 +4,22 @@ window = tk.Tk()
 window.title('图书分类结果')
 window.geometry('640x640')
 
+a = 1
+b = 2
+c = 3
+d = 4
+e = 5
 v = tk.StringVar()
 label = tk.Label(window, textvariable=v,
-                 width=30, height=3,  # width为标签的宽，height为高
-                 font=24,
-                 wraplength=150,  # 设置多少单位后开始换行
-                 anchor='w')  # 设置文本在标签中显示的位置
-v.set("微积分：%d；线性代数：%d；数字逻辑与电路：%d；大学英语：%d；离散数学：%d" % (a, b, c, d, e))
+                 width=300, height=300,  # width为标签的宽，height为高
+                 font=30,
+                 wraplength=600,  # 设置多少单位后开始换行
+                 anchor='w',
+                 justify='left')  # 设置文本在标签中显示的位置
+v.set("微积分：%d；\n"
+      "线性代数：%d；\n"
+      "数字逻辑与电路：%d；\n"
+      "大学英语：%d；\n"
+      "离散数学：%d" % (a, b, c, d, e))
 label.pack()
 window.mainloop()
