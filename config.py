069323ac5dbf3d1c27f4e030a6fe5e0021c61fd7
EMAXfish <162993685+EMAXfish@users.noreply.github.com>
2024-10-13 14:40:02 +0800

    增加跳过权限的代码

---
+++ config.py
@@ -0,0 +1,28 @@
+# 数据库配置相关信息
+sql_msg = {
+    'my_host': 'localhost',
+    'my_root': 'root',
+    'my_password': 'MySQL08091221',
+    'book_database': 'hello'
+}
+
+# 路径相关信息
+path_msg = {
+    "photo_path": r'..\output\book_photo.jpg',
+    "result_path": r"..\output\result.txt",
+    "output_log_path": r'..\output\output.log',
+    "mcu_pre_path": r"..\output\CV.txt",
+    "isp_path": r".\mcu.c",
+    "background_path": r"..\output\init_background.png",
+    "keil_path":r"C:\Keil9.6.1\UV4",
+    "mcu_proj_path":r"C:\Users\30744\Desktop\CodeFiles\MCU_C51\code.uvproj",
+    "mcu_obj_path":r"C:\Users\30744\Desktop\CodeFiles\MCU_C51\Objects"
+}
+book_classes = ['数理基础类', '历史哲学类', '计算机专业类', '小说文学类']
+
+book_names = {
+    '数理基础类': ['微积分', '线性代数'],
+    '历史哲学类': ['论语', '中国近现代史纲要'],
+    '计算机专业类': ['计算机组成与系统结构', '离散数学'],
+    '小说文学类': ['哈利·波特', '朝花夕拾']
+}
